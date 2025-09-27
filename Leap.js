23e29d633cec8b9c2dee7bf2e99faeb2708cf502
ibriiii <ibrahimcollado8@gmail.com>
2025-09-27 23:27:35 +0200

    new file:   Leap.js

---
+++ Leap.js
@@ -0,0 +1,21 @@
+/* 
+Introduction
+A leap year (in the Gregorian calendar) occurs:
+
+In every year that is evenly divisible by 4.
+Unless the year is evenly divisible by 100, in which case it's only a leap year if the year is also evenly divisible by 400.
+Some examples:
+
+1997 was not a leap year as it's not divisible by 4.
+1900 was not a leap year as it's not divisible by 400.
+2000 was a leap year! 
+*/
+
+export const isLeap = (year) => {
+	if (year % 4 === 0) {
+		if (year % 100 === 0 && year % 400 !== 0) return false;
+		return true;
+	}
+
+	return false;
+};
